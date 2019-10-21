773dbd0ba2b8c0d226321230f11f83784eb927b4
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-21 13:40:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,16 +16,16 @@ variable "harbor_instance_ip_region" {}
 variable "harbor_instance_name" {}
 variable "harbor_instance_machine_type" {}
 variable "harbor_instance_zone" {}
-variable "harbor_instance_vpc_name" {}
-variable "harbor_instance_subnet_name" {}
+# variable "harbor_instance_vpc_name" {}
+# variable "harbor_instance_subnet_name" {}
 
 variable "jenkins_instance_ip_name" {}
 variable "jenkins_instance_ip_region" {}
 variable "jenkins_instance_name" {}
 variable "jenkins_instance_machine_type" {}
 variable "jenkins_instance_zone" {}
-variable "jenkins_instance_vpc_name" {}
-variable "jenkins_instance_subnet_name" {}
+# variable "jenkins_instance_vpc_name" {}
+# variable "jenkins_instance_subnet_name" {}
 
 
 variable "sonar_instance_ip_name" {}
@@ -33,8 +33,8 @@ variable "sonar_instance_ip_region" {}
 variable "sonar_instance_name" {}
 variable "sonar_instance_machine_type" {}
 variable "sonar_instance_zone" {}
-variable "sonar_instance_vpc_name" {}
-variable "sonar_instance_subnet_name" {}
+# variable "sonar_instance_vpc_name" {}
+# variable "sonar_instance_subnet_name" {}
 
 
 variable "elk_instance_ip_name" {}
@@ -42,8 +42,8 @@ variable "elk_instance_ip_region" {}
 variable "elk_instance_name" {}
 variable "elk_instance_machine_type" {}
 variable "elk_instance_zone" {}
-variable "elk_instance_vpc_name" {}
-variable "elk_instance_subnet_name" {}
+# variable "elk_instance_vpc_name" {}
+# variable "elk_instance_subnet_name" {}
 
 variable "kube_cluster_name" {}
 variable "kube_cluster_location" {}
