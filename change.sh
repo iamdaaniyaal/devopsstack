7337c0a96f77dd5fd62e79927499ec08896dde75
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-22 19:07:15 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -11,13 +11,13 @@ terraform apply --auto-approve
 
 
 
-if [ $stack==devops ]
-then
-   export jenkins=jk-'$stack-name'-'$stack'-'$time'
-   export jkip=`gcloud compute instances list --filter="name='$jenkins'" --format "get(networkInterfaces[0].accessConfigs[0].natIP)"`; 
-   echo $jkip; 
-   curl -X POST http://"$jkip":8080/job/Gk8/build --user jenkins:jenkins
-fi
+# if [ $stack==devops ]
+# then
+export jenkins=jk-'$stack-name'-'$stack'-'$time'
+export jkip=`gcloud compute instances list --filter="name='$jenkins'" --format "get(networkInterfaces[0].accessConfigs[0].natIP)"`; 
+echo $jkip; 
+curl -X POST http://"$jkip":8080/job/Gk8/build --user jenkins:jenkins
+# fi
 # export jenkins=jk-'$stack-name'-'$stack'-'$time'
 
 # export ip=`gcloud compute instances list --filter="name='$jenkins'" --format "get(networkInterfaces[0].accessConfigs[0].natIP)"`
