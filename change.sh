50dbfbeb3a12dcefd28e9e4e0c88ddc5db069602
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-22 14:57:31 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -8,3 +8,8 @@ sudo sed -i 's/target_machine/'$machine_type'/gI' terraform.tfvars
 terraform init
 terraform plan
 terraform apply --auto-approve
+
+export jenkins=jk-'$stack-name'-'$stack'-'$time'
+
+export ip=gcloud compute instances list --filter="name='$jenkins'" --format "get(networkInterfaces[0].accessConfigs[0].natIP)
+curl -X POST http://'$ip':8080/job/Gk8/build --user jenkins:jenkins
