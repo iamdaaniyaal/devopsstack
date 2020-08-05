413dc8aad5410592a9f71b588d05a993413595f9
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2020-08-05 22:56:50 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -9,7 +9,7 @@ export cluster=$(cut -d + -f 3 <<< $ipdata)
 source ~/.bash_profile
 
 cd /var/lib/jenkins/jobs/Gk8
-sudo sed -i 's/35.227.120.7/'$harborip'/gI' config.xml
+sudo sed -i 's/34.73.203.96/'$harborip'/gI' config.xml
 sudo sed -i 's/k8s-lee-devopsstack-080520145651/'$cluster'/gI' config.xml
 #sudo sed -i 's/https://github.com/ashoksanem/jenkins_test/'
 
@@ -17,7 +17,7 @@ cd /
 cd /var/lib/jenkins
 
 #sudo sed -i 's/34.73.9.146/'$sonarqubeip'/gI' hudson.plugins.sonar.SonarGlobalConfiguration.xml
-sudo sed -i 's/34.73.9.146/34.107.160.153/gI' hudson.plugins.sonar.SonarGlobalConfiguration.xml
+#sudo sed -i 's/34.73.9.146/34.107.160.153/gI' hudson.plugins.sonar.SonarGlobalConfiguration.xml
 sudo sed -i 's/104.196.183.28/'$jenkinsip'/gI' jenkins.model.JenkinsLocationConfiguration.xml
 
 
