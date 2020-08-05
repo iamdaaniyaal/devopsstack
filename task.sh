7c7a9c6d46fc584d92720520c4d786dfaaea4499
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2020-08-05 22:40:28 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -3,14 +3,14 @@ source ~/.bash_profile
 sudo echo 'export ipdata=`curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/description`' >> ~/.bash_profile
 source ~/.bash_profile
 
-export sonarqubeip=$(cut -d + -f 1 <<< $ipdata)
+#export sonarqubeip=$(cut -d + -f 1 <<< $ipdata)
 export harborip=$(cut -d + -f 2 <<< $ipdata)
 export cluster=$(cut -d + -f 3 <<< $ipdata)
 source ~/.bash_profile
 
 cd /var/lib/jenkins/jobs/Gk8
 sudo sed -i 's/35.227.120.7/'$harborip'/gI' config.xml
-sudo sed -i 's/k8s-kumar-devopsstack-072520113630/'$cluster'/gI' config.xml
+sudo sed -i 's/k8s-lee-devopsstack-080520145651/'$cluster'/gI' config.xml
 #sudo sed -i 's/https://github.com/ashoksanem/jenkins_test/'
 
 cd /
@@ -30,10 +30,10 @@ sudo sed -i 's/35.227.120.7/'$harborip'/gI' daemon.json
 sudo systemctl restart docker
 
 cd /opt/sonar-scanner/conf
-sudo sed -i 's/34.74.71.228/'$sonarqubeip'/gI' sonar-scanner.properties
+#sudo sed -i 's/34.74.71.228/'$sonarqubeip'/gI' sonar-scanner.properties
 cd /
 cd /var/lib/jenkins
-sudo sed -i 's/34.74.71.228/'$sonarqubeip'/gI' hudson.plugins.sonar.SonarGlobalConfiguration.xml
+#sudo sed -i 's/34.74.71.228/'$sonarqubeip'/gI' hudson.plugins.sonar.SonarGlobalConfiguration.xml
 
 cat > /opt/ipvalue.txt << EOF
 harbor_target
